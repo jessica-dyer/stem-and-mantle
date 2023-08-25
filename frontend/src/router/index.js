cf5a57d7e0ac12414cdee06443ca5d2c48c94dfb
Jessica Dyer <jessica.dyer@leveltenenergy.com>
2023-08-25 12:45:07 -0700

    changes to frontend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,26 +4,30 @@ import HardcodedForm from "../components/HardcodedForm.vue";
 
 
 const routes = [
-  {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-      {
-        path: 'add-climb', // This is the URL path for the HardcodedForm component
-        name: 'HardcodedForm', // Optional name for the route
-        component: HardcodedForm, // The HardcodedForm component you want to display
-      },
-    ],
-  },
-]
+  { path: "/add-climb", component: HardcodedForm}
+];
+
+// const routes = [
+//   {
+//     path: '/',
+//     component: () => import('@/layouts/default/Default.vue'),
+//     children: [
+//       {
+//         path: '',
+//         name: 'Home',
+//         // route level code-splitting
+//         // this generates a separate chunk (about.[hash].js) for this route
+//         // which is lazy-loaded when the route is visited.
+//         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+//       },
+//       {
+//         path: 'add-climb', // This is the URL path for the HardcodedForm component
+//         name: 'HardcodedForm', // Optional name for the route
+//         component: HardcodedForm, // The HardcodedForm component you want to display
+//       },
+//     ],
+//   },
+// ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
